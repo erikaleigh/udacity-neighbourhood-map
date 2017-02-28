c1b0322d3b20db41023585a673b6d2d32c32fa97
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-27 19:51:22 -0500

    Move initialize function into initMap function

--- js/app.js
+++ js/app.js
@@ -9,34 +9,17 @@ var viewModel =  {
 };
 
 // google maps javascript api
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//       center: {lat: 45.5017, lng: -73.5673},
-//       zoom: 12
-//   });
-// }
+var map;
+var list = $('#list');
+var geocoder;
 
-// error handling for google maps api
-function googleErrorHandling() {
-  var errorMsg = 'Sorry, this app couldn\'t be displayed. Please try again later.';
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.5017, lng: -73.5673},
+      zoom: 12
+  });
 
-  var mapDiv = document.getElementById('map');
-  var errorDiv = document.createElement('p');
-
-  errorDiv.innerHTML = errorMsg;
-  mapDiv.appendChild(errorDiv);
-};
-
-// google maps ajax request to get lat/lng of addresses for markers
-
-
-
-  var list = $('#list');
-  var geocoder;
-  var map;
   // addresses: Patati Patata[0], Qing Hua[1], Majestique[2], Resonance Cafe[3], Dispatch[4], Pikolo[5], Bar le Ritz[6], North Star[7], Brasserie Harricana[8]
-
 
   //  var address = ["4177+StLaurent+Boulevard", "1676+Lincoln+Ave", "4105+StLaurent+Boulevard", "5175A+Ave+DuParc", "267+Rue+StZotique", "3418+Ave+DuParc", "179+Rue+JeanTalonOuest", "3908+StLaurent+Boulevard", "95+Rue+JeanTalonOuest"];
 
@@ -67,3 +50,15 @@ function googleErrorHandling() {
     };
 
   initialize();
+}
+
+// error handling for google maps api
+function googleErrorHandling() {
+  var errorMsg = 'Sorry, this app couldn\'t be displayed. Please try again later.';
+
+  var mapDiv = document.getElementById('map');
+  var errorDiv = document.createElement('p');
+
+  errorDiv.innerHTML = errorMsg;
+  mapDiv.appendChild(errorDiv);
+};
