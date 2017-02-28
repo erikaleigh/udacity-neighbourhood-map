361679e2b4ea1723c887b00380399669180a77cb
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-27 19:21:41 -0500

    Updates to marker/maps initialize function

--- js/app.js
+++ js/app.js
@@ -30,13 +30,15 @@ function googleErrorHandling() {
 
 // google maps ajax request to get lat/lng of addresses for markers
 
-function initMap() {
+
 
   var list = $('#list');
   var geocoder;
   var map;
   // addresses: Patati Patata[0], Qing Hua[1], Majestique[2], Resonance Cafe[3], Dispatch[4], Pikolo[5], Bar le Ritz[6], North Star[7], Brasserie Harricana[8]
-  var address = ["4177+StLaurent+Boulevard", "1676+Lincoln+Ave", "4105+StLaurent+Boulevard", "5175A+Ave+DuParc", "267+Rue+StZotique", "3418+Ave+DuParc", "179+Rue+JeanTalonOuest", "3908+StLaurent+Boulevard", "95+Rue+JeanTalonOuest"];
+
+
+  //  var address = ["4177+StLaurent+Boulevard", "1676+Lincoln+Ave", "4105+StLaurent+Boulevard", "5175A+Ave+DuParc", "267+Rue+StZotique", "3418+Ave+DuParc", "179+Rue+JeanTalonOuest", "3908+StLaurent+Boulevard", "95+Rue+JeanTalonOuest"];
 
   function initialize() {
     geocoder = new google.maps.Geocoder();
@@ -49,31 +51,19 @@ function initMap() {
   }
 
   function codeAddress() {
-    for (i = 0; i < address.length; i++) {
-      var address = address[i];
-      geocoder.geocode( { 'address': address }, function(results,status ) {
-        if (status === 'OK') {
+      var address = "4177 St Laurent Boulevard, Montreal, QC";
+      geocoder.geocode( { 'address': address }, function(results, status) {
+        if (status == 'OK') {
+          console.log(status);
+          map.setCenter(results[0].geometry.location);
           var marker = new google.maps.Marker({
             map: map,
-            position: results[i].geometry.location
+            position: results[0].geometry.location
           });
         } else {
           alert("FAILED BECAUSE" + status);
         }
       });
-    }
-  }
+    };
 
-  //     $.ajax({
-  //       type: "GET",
-  //       url: googleURL,
-  //     }).done(function() {
-  //         list.append();
-  //     }).fail(function() {
-  //         alert('fail');
-  //     });
-  // };
-
-
-};
-initMap();
+  initialize();
