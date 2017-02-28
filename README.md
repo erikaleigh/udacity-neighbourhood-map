a36e239ca8f1031adcf655ecfdcbd40fd7b5a63b
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-28 14:13:32 -0500

    Add markers

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ For completion of [Udacity's Front-End Web Developer Nanodegree](https://www.uda
 **Extra Help**
 - Extra help for AJAX requests using jQuery from Chapter 8 the book [Javascript & jQuery: Interactive Front-End Development](http://javascriptbook.com/) by John Duckett, and [Udacity's Intro to AJAX course](https://www.udacity.com/course/intro-to-ajax--ud110)
 - Used instructions for loading a Google Maps Javascript API from [Google's documentation](https://developers.google.com/maps/documentation/javascript/tutorial)
-- Extra help for setting up markers and info windows from [Udacity's Google Maps API course](https://www.udacity.com/course/google-maps-apis--ud864)
+- Extra help and sample code for setting up markers and info windows from [Udacity's Google Maps API course](https://www.udacity.com/course/google-maps-apis--ud864)
 - Syntax for error handling for Google Maps API load inspired by [this](https://discussions.udacity.com/t/handling-google-maps-in-async-and-fallback/34282/33?) Udacity forum discussion
 - Used parts of example code from [Google's documentation for Geocoding](https://developers.google.com/maps/documentation/javascript/geocoding) for syntax help when calling Geocode locations for markers
--- js/app.js
+++ js/app.js
@@ -3,9 +3,24 @@
 // addresses: Patati Patata[0], Qing Hua[1], Majestique[2], Resonance Cafe[3], Dispatch[4], Pikolo[5], Bar le Ritz[6], North Star[7], Brasserie Harricana[8]
 var Model = [
 
- addresses = ["4177+StLaurent+Boulevard", "1676+Lincoln+Ave", "4105+StLaurent+Boulevard", "5175A+Ave+DuParc", "267+Rue+StZotique", "3418+Ave+DuParc", "179+Rue+JeanTalonOuest", "3908+StLaurent+Boulevard", "95+Rue+JeanTalonOuest"]
+ locations = [
+   {title: "Patati Patata Friterie de Luxe", location: {lat: 45.51808800000001, lng: -73.58135759999999}},
+   {title: "Qing Hua", location: {lat: 45.4953826, lng: -73.58110479999999}},
+   {title: "Majestique", location: {lat: 45.5175626, lng: -73.58028349999999}},
+   {title: "Resonance Café", location: {lat: 45.5204813, lng: -73.597157}},
+   {title: "Café Dispatch", location: {lat: 45.5289229, lng: -73.6160596}},
+   {title: "Café Pikolo", location: {lat: 45.5085961, lng: -73.5717465}},
+   {title: "Bar le Ritz PDB", location: {lat: 45.5326855, lng: -73.6203424}},
+   {title: "North Star Pinball", location: {lat: 45.5163155, lng: -73.5782292}},
+   {title: "Brasserie Harricana", location: {lat: 45.5337701, lng: -73.61937089999999}},
+ ]
+
+
+
+
 
 ]
+
 // view model - data control and storage
 
 var viewModel =  {
@@ -14,6 +29,7 @@ var viewModel =  {
 
 // google maps javascript api map initialization and markers
 var map;
+var markers = [];
 var list = $('#list');
 
 function initMap() {
@@ -21,6 +37,21 @@ function initMap() {
       center: {lat: 45.5017, lng: -73.5673},
       zoom: 12
   });
+
+for (i = 0; i < locations.length; i++) {
+  var position = locations[i].location;
+  var title = locations[i].title;
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: title,
+    animation: google.maps.Animation.DROP,
+  });
+  markers.push(marker);
+}
+
+
 
 // error handling for google maps api
 function googleErrorHandling() {
