9b2e56738154852cb02889727b8364af43cbe40b
Erika LeBlanc <erikalleblanc@gmail.com>
2017-03-07 11:23:16 -0500

    Add styling, list view of markers

--- js/app.js
+++ js/app.js
@@ -1,77 +1,3 @@
-/*
- * GOOGLE MAPS
- * Use JavaScript API to initialize map, markers and info windows
- */
-
-var map;
-var markers = [];
-
-// Initialize Google Map
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 45.5017, lng: -73.5673},
-      zoom: 13
-  });
-// Set transit layer styling to make metro lines more visible
-  var transitLayer = new google.maps.TransitLayer();
-         transitLayer.setMap(map);
-
-// Initialize info windows
-var infoWindow = new google.maps.InfoWindow();
-var bounds = new google.maps.LatLngBounds();
-
-// Add markers from locations listed in model - var locations
-for (i = 0; i < locations.length; i++) {
-  var position = locations[i].location;
-  var title = locations[i].title;
-  var markerImg = 'img/camera.png';
-
-  var marker = new google.maps.Marker({
-    map: map,
-    position: position,
-    title: title,
-    icon: markerImg,
-    animation: google.maps.Animation.DROP,
-  });
-  markers.push(marker);
-  bounds.extend(marker.position);
-
-/* Open info window and change marker to flash icon when marker is clicked
- * 'this' = marker
- */
-  marker.addListener('click', function() {
-    populateInfoWindow(this, infoWindow);
-    this.setIcon('img/flash.png');
-  });
-
-/* Populate infowindow with marker title when the marker is clicked
- * Check to make sure info window is not already open before performing
- */
-  function populateInfoWindow(marker, infowindow) {
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
-      infowindow.open(map, marker);
-      infowindow.addListener('closeclick', function() {
-        infowindow.setMarker = null;
-        marker.setIcon('img/camera.png');
-      });
-    }
-  }
-  map.fitBounds(bounds);
-}
-
-// Error handling for google maps api
-function googleErrorHandling() {
-  var errorMsg = 'Sorry, this app couldn\'t be displayed. Please try again later.';
-
-  var mapDiv = document.getElementById('map');
-  var errorDiv = document.createElement('p');
-
-  errorDiv.innerHTML = errorMsg;
-  mapDiv.appendChild(errorDiv);
-}
-};
 
 // Model - data storage
 
@@ -90,24 +16,29 @@ function googleErrorHandling() {
    {title: "Metro St-Michel", location: {lat: 45.5599217, lng: -73.6000536}},
 ];
 
-var Entry = function(data) {
+var Location = function(data) {
   this.title = ko.observable(data.title);
 }
+
 
 // View Model - data control and storage
 
 var ViewModel = function() {
 
+/* Push the titles of all items in var locations to an observable array,
+ * bound to the list view
+ */
   var self = this;
+  this.locationList = ko.observableArray([]);
 
-this.list = ko.observableArray([]);
-
-locations.forEach(function(entryItem) {
-  self.list.push( new Entry (entryItem))
+  locations.forEach(function(locationItem) {
+    self.locationList.push( new Location(locationItem));
 });
-
-  this.currentEntry = ko.observable(this.list()[0]);
-
+  this.currentLocation = ko.observable(this.locationList()[0]);
 };
 
+
+
+
+// Apply Knockout.js bindings
 ko.applyBindings(new ViewModel());
