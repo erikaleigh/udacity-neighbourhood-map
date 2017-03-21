20e8e8db63bf8e060363c4273a68826dadb08695
Erika LeBlanc <erikalleblanc@gmail.com>
2017-03-21 17:04:56 -0400

    Beautify code

--- js/map.js
+++ js/map.js
@@ -9,122 +9,126 @@ var markers = [];
 
 // Initialize function from callback
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 45.5017, lng: -73.5673},
-      zoom: 13,
-      styles: [
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#A7CAD7'}]
-        }
-      ]
-  });
-// Set transit layer styling to make metro lines more visible
-  var transitLayer = new google.maps.TransitLayer();
-         transitLayer.setMap(map);
+ map = new google.maps.Map(document.getElementById('map'), {
+  center: {
+   lat: 45.5017,
+   lng: -73.5673
+  },
+  zoom: 13,
+  styles: [{
+   featureType: 'water',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#A7CAD7'
+   }]
+  }]
+ });
+ // Set transit layer styling to make metro lines more visible
+ var transitLayer = new google.maps.TransitLayer();
+ transitLayer.setMap(map);
 
-// Initialize info windows and map bounds
-var infoWindow = new google.maps.InfoWindow(
-  {
-    maxWidth: 150
-  });
-var bounds = new google.maps.LatLngBounds();
+ // Initialize info windows and map bounds
+ var infoWindow = new google.maps.InfoWindow({
+  maxWidth: 150
+ });
+ var bounds = new google.maps.LatLngBounds();
 
-// Add markers from locations listed in model - var locations
-for (i = 0; i < locations.length; i++) {
+ // Add markers from locations listed in model - var locations
+ for (i = 0; i < locations.length; i++) {
   var position = locations[i].location;
   var title = locations[i].title;
   var markerImg = 'img/camera.png';
 
   marker = new google.maps.Marker({
-    map: map,
-    position: position,
-    title: title,
-    icon: markerImg,
-    animation: google.maps.Animation.DROP,
+   map: map,
+   position: position,
+   title: title,
+   icon: markerImg,
+   animation: google.maps.Animation.DROP,
   });
   bounds.extend(marker.position);
   vm.locationList()[i].marker = marker;
 
-// Open info window and change marker to flash icon when marker is clicked 'this' = marker
+  // Open info window and change marker to flash icon when marker is clicked 'this' = marker
   marker.addListener('click', function() {
-    populateInfoWindow(this, infoWindow);
-    this.setIcon('img/flash.png');
+   populateInfoWindow(this, infoWindow);
+   this.setIcon('img/flash.png');
   });
 
-// Populate infowindow with marker title & wikipedia API info when the marker is clicked
+  // Populate infowindow with marker title & wikipedia API info when the marker is clicked
   function populateInfoWindow(marker, infowindow) {
 
-    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '_(Montreal_Metro)&imlimit=5&format=json&callback=wikiCallback';
+   var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '_(Montreal_Metro)&imlimit=5&format=json&callback=wikiCallback';
 
-  // Wikipedia AJAX Request to add Wikipedia entry on selected metro station to infoWindow
-    $.ajax({
-      url: wikiUrl,
-      dataType: 'jsonp'
-    }).done(function(data) {
+   // Wikipedia AJAX Request to add Wikipedia entry on selected metro station to infoWindow
+   $.ajax({
+    url: wikiUrl,
+    dataType: 'jsonp'
+   }).done(function(data) {
 
-      var articleUrl = data[3][0];
-      var articleDescr = data[2][0];
+    var articleUrl = data[3][0];
+    var articleDescr = data[2][0];
 
-      // Error handling for if no articles are returned from Wikipedia API
-      if (articleUrl === undefined) {
-        infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + 'Sorry no wikipedia entries could be found to match this station.' + '</p>'+ '</div>');
-        infowindow.open(map, marker);
+    // Error handling for if no articles are returned from Wikipedia API
+    if (articleUrl === undefined) {
+     infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + 'Sorry no wikipedia entries could be found to match this station.' + '</p>' + '</div>');
+     infowindow.open(map, marker);
 
-      }
-      else {
+    } else {
 
-              infowindow.marker = marker;
-              infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + articleDescr + '<a href="' + articleUrl + '" target="blank">' + '..' + ' Read More'+ '</a>' + '</p>' + '</div>');
-              infowindow.open(map, marker);
-      }
+     infowindow.marker = marker;
+     infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + articleDescr + '<a href="' + articleUrl + '" target="blank">' + '..' + ' Read More' + '</a>' + '</p>' + '</div>');
+     infowindow.open(map, marker);
+    }
 
     // Error handling for if Wikipedia API call fails
 
-      }).fail(function() {
-        infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + 'Sorry no wikipedia entries could be found to match this station.' + '</p>'+ '</div>');
-        infowindow.open(map, marker);
+   }).fail(function() {
+    infowindow.setContent('<div>' + '<h3>' + marker.title + '</h3>' + '<p>' + 'Sorry no wikipedia entries could be found to match this station.' + '</p>' + '</div>');
+    infowindow.open(map, marker);
 
-    });
+   });
 
-  // Google Maps event listeners:
-  // Close info window and change marker back to camera when user clicks on the // x in the infoWindow or anywhere on the map, reset map center for window
-  // resize
-    google.maps.event.addListener(map, 'click', function() {
-      infowindow.close();
-      infowindow.setMarker = null;
-      marker.setIcon('img/camera.png');
+   // Google Maps event listeners:
+   // Close info window and change marker back to camera when user clicks on the // x in the infoWindow or anywhere on the map, reset map center for window
+   // resize
+   google.maps.event.addListener(map, 'click', function() {
+    infowindow.close();
+    infowindow.setMarker = null;
+    marker.setIcon('img/camera.png');
+   });
+   google.maps.event.addListener(infowindow, 'closeclick', function() {
+    infowindow.close();
+    infowindow.setMarker = null;
+    marker.setIcon('img/camera.png');
+   });
+   google.maps.event.addDomListener(window, 'resize', function() {
+    console.log('resize');
+    map.setCenter({
+     lat: 45.501556,
+     lng: -73.567621
     });
-    google.maps.event.addListener(infowindow, 'closeclick', function() {
-      infowindow.close();
-      infowindow.setMarker = null;
-      marker.setIcon('img/camera.png');
-  });
-    google.maps.event.addDomListener(window, 'resize', function() {
-      console.log('resize');
-      map.setCenter({lat:45.501556, lng:-73.567621});
-      map.fitBounds(bounds);
-  });
+    map.fitBounds(bounds);
+   });
   }
   map.fitBounds(bounds);
-}
+ }
 
-// Apply Knockout.js bindings
-ko.applyBindings(vm);
+ // Apply Knockout.js bindings
+ ko.applyBindings(vm);
 };
 
 // Error handling for google maps api
 
 function mapErrorHandling() {
-    var errorMsg = 'Sorry, no worky. Please refresh and try again!';
+ var errorMsg = 'Sorry, no worky. Please refresh and try again!';
 
-    var mapDiv = document.getElementById('map');
-    var errorDiv = document.createElement('p');
-    errorDiv.innerHTML = errorMsg;
-    mapDiv.appendChild(errorDiv);
+ var mapDiv = document.getElementById('map');
+ var errorDiv = document.createElement('p');
+ errorDiv.innerHTML = errorMsg;
+ mapDiv.appendChild(errorDiv);
 
-    var errorImg = document.createElement('img');
-    errorImg.src = 'img/sad.png';
-    mapDiv.appendChild(errorImg);
+ var errorImg = document.createElement('img');
+ errorImg.src = 'img/sad.png';
+ mapDiv.appendChild(errorImg);
 };
--- js/viewModel.js
+++ js/viewModel.js
@@ -1,64 +1,62 @@
 // View Model - data control and storage
 
 var ViewModel = function() {
-  var self = this;
+ var self = this;
 
  // Set up a Location constructor to be used to set the locations to the list view as well as for markers
  var Location = function(data) {
-   this.title = data.title;
-   this.line = data.line;
-   this.marker = data.marker;
-   this.isVisible = ko.observable(true);
+  this.title = data.title;
+  this.line = data.line;
+  this.marker = data.marker;
+  this.isVisible = ko.observable(true);
  };
 
-  self.locationList = ko.observableArray([]);
+ self.locationList = ko.observableArray([]);
 
-// Push all locations to an array - locationList
+ // Push all locations to an array - locationList
 
-  locations.forEach(function(locationItem) {
-    self.locationList.push( new Location(locationItem));
-  });
+ locations.forEach(function(locationItem) {
+  self.locationList.push(new Location(locationItem));
+ });
 
-// Set up dropdown menu options for filtering the list view and markers
+ // Set up dropdown menu options for filtering the list view and markers
 
-  self.lineOptions = ['all lines', 'orange', 'green', 'blue', 'yellow'];
+ self.lineOptions = ['all lines', 'orange', 'green', 'blue', 'yellow'];
 
-// Initialize the selectedLine as "all lines" to set default of all list items and markers showing upon load
+ // Initialize the selectedLine as "all lines" to set default of all list items and markers showing upon load
 
-  self.selectedLine = ko.observable(self.lineOptions[0]);
+ self.selectedLine = ko.observable(self.lineOptions[0]);
 
-// Filter list items corresponding markers according to their .line property (which metro line they fall on). Toggle visibility if corresponding line is selected
+ // Filter list items corresponding markers according to their .line property (which metro line they fall on). Toggle visibility if corresponding line is selected
 
-  self.filterItems = ko.computed(function() {
-    var listItem = self.locationList();
-    var selectedLine = self.selectedLine();
-    for (var i = 0; i < listItem.length; i++) {
-      if (selectedLine === self.lineOptions[0]) {
-        listItem[i].isVisible(true);
-        if (marker) {
-          listItem[i].marker.setVisible(true);
-        }
-
-      }
-      else if (selectedLine !== listItem[i].line) {
-        listItem[i].isVisible(false);
-        listItem[i].marker.setVisible(false);
-      }
-      else {
-        listItem[i].isVisible(true);
-        listItem[i].marker.setVisible(true);
-      }
+ self.filterItems = ko.computed(function() {
+  var listItem = self.locationList();
+  var selectedLine = self.selectedLine();
+  for (var i = 0; i < listItem.length; i++) {
+   if (selectedLine === self.lineOptions[0]) {
+    listItem[i].isVisible(true);
+    if (marker) {
+     listItem[i].marker.setVisible(true);
     }
-  });
 
-  self.markers = ko.observableArray([]);
+   } else if (selectedLine !== listItem[i].line) {
+    listItem[i].isVisible(false);
+    listItem[i].marker.setVisible(false);
+   } else {
+    listItem[i].isVisible(true);
+    listItem[i].marker.setVisible(true);
+   }
+  }
+ });
 
-//  Initiate google event to open infoWindow when list item is clicked
-  self.openWindow = function(place){
-    google.maps.event.trigger(place.marker, 'click');
-  };
+ self.markers = ko.observableArray([]);
 
-// Close view model
+ //  Initiate google event to open infoWindow when list item is clicked
+ self.openWindow = function(place) {
+  google.maps.event.trigger(place.marker, 'click');
+ };
+
+ // Close view model
 };
 // Store ViewModel in vm variable for instantiation in map.js
 var vm = new ViewModel();
