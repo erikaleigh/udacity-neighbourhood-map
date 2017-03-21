947e7ed3c2c238b3848ed998207411703feaaeb0
Erika LeBlanc <erikalleblanc@gmail.com>
2017-03-20 21:48:54 -0400

    Add media query

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,7 +1,11 @@
 // jQuery event for toggling mobile nav in and out
 
 $('.hamburger').on('click', function() {
-  $('.list-box').toggleClass('list-box-open')
+  $('.list-box').toggleClass('list-box-open');
 });
 
-//
+// close mobile nav when list item is clicked
+
+$('.list-item').on('click', function() {
+  $('list-box').toggleClass('list-box-open');
+});
